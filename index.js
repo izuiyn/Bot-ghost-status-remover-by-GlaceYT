adb20cb0b28620724cbd1a9b04fe66da1188d57a
izuiyn <groverpihu07@gmail.com>
2024-11-13 14:40:22 +0530

    Update index.js
    updatee

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["🍥 watching over sawako shop !", "🍥 handling tickets in sawako !"];
+const statusTypes = [ 'idle', 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
